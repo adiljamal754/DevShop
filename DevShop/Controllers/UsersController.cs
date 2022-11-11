a3c42ee03aa412e32ca2564bd23ff8b03d9cef63
adiljamal754 <https://github.com/adiljamal754>
2022-11-11 21:24:39 +0200

    added Login and SingUp Views, and the routes

--- DevShop/Controllers/UsersController.cs
+++ DevShop/Controllers/UsersController.cs
@@ -1,10 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevShop.Models;
+using DevShop.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevShop.Controllers
 {
     public class UsersController : Controller
     {
-        public IActionResult View()
+        private readonly ILogger<HomeController> _logger;
+        private readonly SMSService _SMSService;
+        private readonly UserService _userService;
+
+
+        public UsersController(ILogger<HomeController> logger, SMSService sMSService, UserService userService)
+        {
+            _logger = logger;
+            _SMSService = sMSService;
+            _userService = userService;
+
+
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(User user)
+        {
+            _userService.AddUser(user);
+            _SMSService.sendSMS(user.Contact, user.Username);
+            return RedirectToAction(nameof(Index));
+            // View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
             return View();
         }
